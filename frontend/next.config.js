8a40a8d179243422f1893afee6d64d3e4d682b2e
zzuts <zzuts@mail.pollo.ai>
2026-03-22 03:37:01 +0800

    feat: 接入 cocze loop

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,7 +3,7 @@ module.exports = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:8888/api/:path*',
+        destination: 'http://localhost:8000/api/:path*',
       },
     ];
   },
